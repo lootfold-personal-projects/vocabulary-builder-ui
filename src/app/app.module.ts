8af666032b0d2952234e9c24f1deecd9258e000d
lootfold <pallav.92@gmail.com>
2020-02-05 00:49:52 +0530

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ItemListComponent } from './items/item-list/item-list.component';
 import { AddItemComponent } from './items/add-item/add-item.component';
@@ -11,16 +12,19 @@ import { ViewItemComponent } from './items/view-item/view-item.component';
 import { ItemsService } from './items/items.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
     AddItemComponent,
-    ViewItemComponent
+    ViewItemComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
